8a7f235d1eca77cf3433c7e792822bbd7e20bc01
Paul Davila <design@abclogistica.mx>
2026-06-30 14:31:28 -0500

    Update firmas landing for assisted WhatsApp sales

--- firmas/cta.js
+++ firmas/cta.js
@@ -2,6 +2,6 @@
 window.BOQI_FIRMAS = {
   messengerUrl: "https://m.me/61579412811926",
   whatsappE164: "593963173441",
-  whatsappText: "Hola, quiero tramitar mi firma electrónica con Boqi",
+  whatsappText: "Hola, quiero comprar una firma electrónica con Boqi. Por favor ayúdenme con los planes y el acceso privado.",
   solicitarUrl: "https://boqi-firmas-panel.pages.dev/#/solicitar",
 };
